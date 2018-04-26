542a285cd2ca7bff22594cc9bc3711292f964071
Roman Matveev <roman.x@live.com>
2018-04-26 11:53:19 -0400

    Added GET and POST functionality.

--- gameapp/user/views.py
+++ gameapp/user/views.py
@@ -6,11 +6,25 @@ from user.models import User
 from django.views.decorators.csrf import csrf_exempt
 
 class HomePageView(APIView):
-    def get(self, request=None, uname="test", format=None):
-        try:
-            found_user = User.objects.get(name=uname)
-        except ObjectDoesNotExist as e:
-            return HttpResponse(json.dumps({"status":"NoSuchUser"}), status=404)
+
+    @csrf_exempt
+    def create_or_retrieve(self, request=None, uname="test", format=None):
+        if request.method == "GET":
+            try:
+                found_user = User.objects.get(name=uname)
+                return HttpResponse(json.dumps({"status":"Success"}), status=200)
+            except ObjectDoesNotExist as e:
+                return HttpResponse(json.dumps({"status":"NoSuchUser"}), status=404)
+
+        if request.method == "POST":
+            try:
+                found_user = User.objects.get(name=uname)
+                return HttpResponse(json.dumps({"status":"AlreadyExists"}), status=403)
+            except ObjectDoesNotExist as e:
+                pass
+            u = User(name=uname)
+            u.save()
+            return HttpResponse(json.dumps({"status":"Success"}))
 
         data = { "user": uname, "id": found_user.id }
         return HttpResponse(json.dumps(data))
